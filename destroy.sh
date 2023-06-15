f164c518f4c1f8a85af94277d46eeee5b0d81430
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2023-06-15 18:05:47 +0530

    Create destroy.sh

---
+++ destroy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+terraform -chdir=02-lbc-install-terraform-manifests/ destroy --auto-approve
+terraform -chdir=01-ekscluster-terraform-manifests/ destroy --auto-approve
+
+aws dynamodb delete-table --table-name abdnikam
+aws dynamodb delete-table --table-name abdnikam-lbc
+#aws s3 rb s3://abdnikamm --force
