e0d872194de56a2d20eb0f70b53ad050747fd56b
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2023-06-15 11:02:22 +0530

    Update bucket.sh

--- bucket.sh
+++ bucket.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export AWS_DEFAULT_REGION=us-east-1
 aws s3api create-bucket --bucket abdnikam2
 aws s3api put-object --bucket abdnikam2 --key dev/eks-cluster/
 aws s3api put-object --bucket abdnikam2 --key dev/aws-lbc/
